39fc7af856708935b5522c78a3e4ce537e6b0765
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-28 09:49:07 -0400

    Add index.html template for the web application and installed Bootstrap

--- ManejoFlask/app.py
+++ ManejoFlask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app=Flask(__name__)#Flask es una clase que se importa del módulo flask, esta clase se utiliza para crear una aplicación web, el argumento __name__ es el nombre del módulo actual, esto le dice a Flask dónde buscar los archivos estáticos y las plantillas.
 
@@ -7,7 +7,7 @@ app=Flask(__name__)#Flask es una clase que se importa del módulo flask, esta cl
 
 def inicio():
   app.logger.debug('Se ha accedido a la ruta /') #logger es una herramienta de depuración que nos permite registrar mensajes en la consola, es el nivel debug es el más bajo, es decir, se registrarán todos los mensajes, incluyendo los de depuración.
-  return 'Hola Mundo'
+  return render_template('index.html') #render_template es una función que se importa del módulo flask, esta función se utiliza para renderizar una plantilla HTML, el argumento 'index.html' es el nombre de la plantilla que se va a renderizar, esta plantilla debe estar ubicada en la carpeta templates.
 
 if __name__ == '__main__':
   app.run(debug=True)
