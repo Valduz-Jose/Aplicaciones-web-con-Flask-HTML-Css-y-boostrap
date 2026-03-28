dd36b9d8da3d9f4923bb97589fcb3ebba3f47e7e
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-28 10:31:34 -0400

    Implement Cliente model and DAO for database operations; add connection pooling for MySQL

--- ManejoFlask/app.py
+++ ManejoFlask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+from cliente_dao import ClienteDAO
 
 app=Flask(__name__)#Flask es una clase que se importa del módulo flask, esta clase se utiliza para crear una aplicación web, el argumento __name__ es el nombre del módulo actual, esto le dice a Flask dónde buscar los archivos estáticos y las plantillas.
 titulo_app = 'Zona Fit (GYM)'
@@ -8,7 +9,12 @@ titulo_app = 'Zona Fit (GYM)'
 
 def inicio():
   app.logger.debug('Se ha accedido a la ruta /') #logger es una herramienta de depuración que nos permite registrar mensajes en la consola, es el nivel debug es el más bajo, es decir, se registrarán todos los mensajes, incluyendo los de depuración.
-  return render_template('index.html', titulo=titulo_app) #render_template es una función que se importa del módulo flask, esta función se utiliza para renderizar una plantilla HTML, el argumento 'index.html' es el nombre de la plantilla que se va a renderizar, esta plantilla debe estar ubicada en la carpeta templates.
+
+# Recuperamos clientes en la BD
+  clientes_db = ClienteDAO.seleccionar()
+
+
+  return render_template('index.html', titulo=titulo_app, clientes=clientes_db) #render_template es una función que se importa del módulo flask, esta función se utiliza para renderizar una plantilla HTML, el argumento 'index.html' es el nombre de la plantilla que se va a renderizar, esta plantilla debe estar ubicada en la carpeta templates.
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ ManejoFlask/cliente_dao.py
@@ -0,0 +1,104 @@
+from conexion import Conexion
+from cliente import Cliente
+
+
+class ClienteDAO:
+    SELECCIONAR = "SELECT * FROM cliente ORDER BY id"
+    INSERTAR = "INSERT INTO cliente(nombre, apellido, membresia) VALUES(%s, %s, %s)"
+    ACTUALIZAR = "UPDATE cliente SET nombre=%s, apellido=%s, membresia=%s WHERE id=%s"
+    ELIMINAR = "DELETE FROM cliente WHERE id=%s"
+
+    @classmethod
+    def seleccionar(cls):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+            cursor.execute(cls.SELECCIONAR)
+            registros = cursor.fetchall()
+            # Mapeo de clase-tabla cliente
+            clientes = []
+            for registro in registros:
+                cliente = Cliente(registro[0], registro[1], registro[2], registro[3])
+                # print(cliente)
+                clientes.append(cliente)
+            return clientes
+        except Exception as e:
+            print(f"Error al seleccionar clientes: {e}")
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def insertar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (cliente.nombre, cliente.apellido, cliente.membresia)
+            cursor.execute(cls.INSERTAR, valores)
+            conexion.commit()
+            return cursor.rowcount#Valores modificados en la base de datos
+        except Exception as e:
+            print(f"Error al insertar cliente: {e}")
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def actualizar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (cliente.nombre, cliente.apellido, cliente.membresia, cliente.id)
+            cursor.execute(cls.ACTUALIZAR, valores)
+            conexion.commit()
+            return cursor.rowcount#Valores modificados en la base de datos
+        except Exception as e:
+            print(f"Error al actualizar cliente: {e}")
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+    @classmethod
+    def eliminar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+            valores = (cliente.id,)
+            cursor.execute(cls.ELIMINAR, valores)
+            conexion.commit()
+            return cursor.rowcount#Valores modificados en la base de datos
+        except Exception as e:
+            print(f"Error al eliminar cliente: {e}")
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
+
+# Pruebas
+if __name__ == "__main__":
+    # # Insertar clientes
+    # print("-"*50)
+    # cliente1 = Cliente(nombre="Juan", apellido="Perez", membresia=300)
+    # clientes_actualizados = ClienteDAO.insertar(cliente1)
+    # print(f"Clientes insertados: {clientes_actualizados}")
+    # print("-"*50)
+    #Actualizar CLiente
+    # cliente_actualizar = Cliente(2,"Alexa", "Perez", 600)
+    # clientes_actualizados = ClienteDAO.actualizar(cliente_actualizar)
+    # print(f"Clientes actualizados: {clientes_actualizados}")
+    # Eliminar cliente
+    cliente_eliminar = Cliente(id=3)
+    clientes_eliminados = ClienteDAO.eliminar(cliente_eliminar)
+    print(f"Clientes eliminados: {clientes_eliminados}")
+    # seleccionar clientes
+    clientes = ClienteDAO.seleccionar()
+    for cliente in clientes:
+        print(cliente)
